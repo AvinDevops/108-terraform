a72d5462812ff9f0f1c042831332491a527b512b
AvinDevops <avinashbandela73@gmail.com>
2026-09-15 22:07:17 +0530

    creating ec2 using tf

---
+++ 01-ec2/ec2.tf
@@ -0,0 +1,35 @@
+# creating ec2 instance
+resource "aws_instance" "db" {
+    ami = "ami-0220d79f3f480ecf5"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+    tags = {
+        Name = "Db"
+    }
+}
+
+# creating security group
+resource "aws_security_group" "allow_ssh" {
+    name = "allow_ssh"
+    description = "allowing ssh only"
+    
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow_ssh"
+    }
+}
+
